7a5a23d480baa1418352e8f19906c896725e0819
Mohadese Salehi <mohadesesalehiiii1380@gmail.com>
2022-12-28 14:17:27 +0330

    Added CarHorn
    Added car horn when you press space key

---
+++ Assets/Scripts/PlayOnSpaceBar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnSpaceBar : MonoBehaviour
+{
+
+    public  AudioSource carHorn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            carHorn.Play();
+        }        
+    }
+}
